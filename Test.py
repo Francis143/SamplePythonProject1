c1425e7659892625215cd290a544c5fa244c1628
francis <francismaanrsa@gmail.com>
2025-08-15 23:56:14 +0530

    Update Strings Predefined Methods

--- Test.py
+++ Test.py
@@ -1,1 +1,21 @@
-name="Francis"
+str="Welcome to  India"
+
+#Find a Length of the String
+
+print(len(str))
+
+# Replace the String 
+
+print(str.replace("to","To"))
+
+# Slice the String
+
+print(str[2:5])
+
+# Slice the From Start
+
+print(str[3:])
+
+# Slice the From End
+
+print(str[:8])
